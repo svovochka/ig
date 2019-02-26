b72c806bbc7671794a08e293850d6828e219f2ff
Vladimir M Semenov <semenov@magora-systems.com>
2019-02-26 20:55:02 +0700

    Pages content

--- src/js/emulator.js
+++ src/js/emulator.js
@@ -1,5 +1,8 @@
 $(function () {
 
+    var googleApiKEy = 'AIzaSyAeaCdWEoguKgVKsPGEDMTIHRTHdf67iO0';
+    var googleApiKEy = 'AIzaSyBCMn3GeAkyi7CeXcq7PDkmlhz039LKnVw';
+
     $('.dropdown').click(function (e) {
         var dd = $(e.target);
         if (!dd.hasClass('.dropdown')) {
@@ -9,6 +12,29 @@ $(function () {
         e.preventDefault();
         e.stopPropagation();
         return false;
+    });
+
+    $('.checkbox').click(function (e) {
+        var dd = $(e.target);
+        if (!dd.hasClass('.checkbox')) {
+            dd = dd.parents('.checkbox');
+        }
+        dd.toggleClass('checked');
+        e.preventDefault();
+        e.stopPropagation();
+        return false;
+    });
+
+    $('#toggle-notifications').click(function () {
+        var nfs = $('#notifications');
+        if (nfs.length > 0) {
+            if (nfs.css('display') === 'none') {
+                nfs.show();
+            } else {
+                nfs.hide();
+            }
+        }
+
     });
 
     var win = $(window);
@@ -21,3 +47,13 @@ $(function () {
     }
 
 });
+
+
+var map;
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 59.9316203, lng: 30.3041552},
+        zoom: 13
+    });
+}
